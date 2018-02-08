0237b98d0b76c603bb002aca715bbcc113443d86
Anton Zdanov <akumael@gmail.com>
2018-02-08 23:52:15 +0200

    refactor: create shared directory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
-import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
+import { MaterialModule } from "./shared/app-material.module";
 import { AppRoutingModule } from "./app-routing.module";
-
 import { AppComponent } from "./app.component";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MaterialModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
